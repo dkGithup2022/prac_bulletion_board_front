0befa2a603a1b7b8c417bc0521e1fc0cbf145189
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-01-28 16:31:10 +0900

    리액트앱  초기 설정

---
+++ src/pages/home/Home.js
@@ -0,0 +1,17 @@
+import "./home.scss";
+
+const Home = () => {
+ return (
+    <div className="home-wrapper">
+        <div className="home-title">
+            <span> DK</span> 에 오신것을 환영합니다.
+        </div>
+        <div className="about-project">
+            이 프로젝트는 "덕구공" 블로그를 참고하여 만들었습니다. 
+        </div>
+    </div>
+    
+ )
+}
+
+export default Home;
